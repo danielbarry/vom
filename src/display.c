f73902c4a1c60da8aef2a35bf51459070e68ec6d
Dan <danbarry16@googlemail.com>
2016-04-20 15:43:53 +0100

    Add source basic code

---
+++ src/display.c
@@ -0,0 +1,58 @@
+#include <curses.h>
+
+/* Methods */
+void display_init();
+void display_update();
+char display_get_char();
+void display_quit();
+void display_update_dimensions();
+void display_draw(unsigned int x, unsigned int y, char c);
+
+/* Variables */
+WINDOW* window;
+unsigned int width;
+unsigned int height;
+
+void display_init(){
+  /* Initialise the display */
+  window = initscr();
+  /* Turn off wait for enter key */
+  cbreak();
+  /* Stop echo to the terminal */
+  noecho();
+  /* Clear the display */
+  clear();
+}
+
+void display_update(){
+  /* Update the display dimensions */
+  display_update_dimensions();
+  /* TODO: Write this code. */
+  display_draw(0, 0, '#'); // TODO: Remove me.
+  /* Refresh the display */
+  refresh();
+}
+
+char display_get_char(){
+  /* Returns a character */
+  return getch();
+}
+
+void display_quit(){
+  /* Restore the original window */
+  endwin();
+}
+
+void display_update_dimensions(){
+  /* Update the width and height */
+  getmaxyx(window, height, width);
+}
+
+void display_draw(unsigned int x, unsigned int y, char c){
+  /* Get character to given position */
+  move(x, y);
+  /* Delete the character currently in the position */
+  delch();
+  /* Insert a new character into the new position */
+  insch(c);
+}
---
+++ src/main.c
@@ -0,0 +1,40 @@
+#include "display.c"
+
+/* Methods */
+void main_init();
+void main_loop();
+
+/* Variables */
+bool running;
+
+/**
+ * main()
+ *
+ * The main entry method into the program.
+ *
+ * @param argc The number of arguments parsed to the program.
+ * @param argv The parameters that were passed to the program.
+ **/
+int main(int argc, char** argv){
+  main_init();
+  main_loop();
+}
+
+void main_init(){
+  running = true;
+}
+
+void main_loop(){
+  /* Initialise the display */
+  display_init();
+  /* TODO: Write this code. */
+  while(running){
+    display_update();
+    char chr = display_get_char();
+    if(chr == 'q'){
+      running = false;
+    }
+  }
+  /* Release the display */
+  display_quit();
+}
